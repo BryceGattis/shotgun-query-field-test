fad55c3ee0aa5b273950e9a351661bfca859d75a
Bryce Gattis <brycegattis@yahoo.com>
2025-05-29 08:58:54 -0500

    Add failing test to ensure that SG instance can be created
    Signed-off-by: Bryce Gattis <brycegattis@yahoo.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
-import pytest
+import shotgun_query_field_test.main
+import shotgun_api3
 
-def test_assert_true():
-    assert True
+def test_sg_instance_created():
+    sg = shotgun_query_field_test.main.get_shotgun_instance()
+    assert isinstance(sg, shotgun_api3.Shotgun)
