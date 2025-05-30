e746d772ec7c04299732132087ce28ed33b0aa79
BryceGattis <brycegattis@yahoo.com>
2025-05-30 18:01:14 -0500

    Start breaking up bigger functions and remove redundant code
    Signed-off-by: BryceGattis <brycegattis@yahoo.com>

--- src/shotgun_query_field_test/main.py
+++ src/shotgun_query_field_test/main.py
@@ -21,15 +21,12 @@ def get_shotgun_instance() -> shotgun_api3.Shotgun:
 def get_query_field_value(entity_type: str, field_name: str, entity_id: int) -> str:
     sg = get_shotgun_instance()
     schema = sg.schema_field_read(entity_type, field_name)
-    # Excellent debugging tool for this kind of job.
-    pprint.pprint(schema)
     if not schema:
         raise ValueError(f"Unable to find field: {field_name} on entity type: {entity_type}")
     properties = schema[field_name]['properties']
-    filters = properties['query']['value']['filters']
-    converted_filters = _convert_query_filters_to_queryable_format(filters, entity_id)
+    query_filters = properties['query']['value']['filters']
+    converted_filters = _convert_query_filters_to_queryable_format(query_filters, entity_id)
     converted_filters = [converted_filters]
-    pprint.pprint(converted_filters)
     query_entity_type = properties['query']['value']['entity_type']
     query_function_for_summary_type = _get_query_function_for_summary_type(properties['summary_default']['value'])
     query_field_value = query_function_for_summary_type(
@@ -46,18 +43,27 @@ def _convert_query_filters_to_queryable_format(filters: Dict[str, Any],
     logical_operator = filters.get('logical_operator')
     is_complex_filter = True if logical_operator else False
     if is_complex_filter:
-        filter_operator = 'all' if 'and' in logical_operator else 'any'
         child_filters = filters['conditions']
-        converted_filters = []
-        for child_filter in child_filters:
-            converted_filter = _convert_query_filters_to_queryable_format(child_filter, entity_id)
-            converted_filters.append(converted_filter)
-        queryable_filter = {
-            "filter_operator": filter_operator,
-            "filters": converted_filters
-        }
-        return queryable_filter
-    # This is not a complex filter.
+        return _convert_complex_query_filters_to_queryable_format(logical_operator, child_filters, entity_id)
+    return _convert_simple_query_filters_to_queryable_format(filters, entity_id)
+
+
+def _convert_complex_query_filters_to_queryable_format(logical_operator: str, child_filters: List[Dict[str, Any]],
+                                                       entity_id: int) -> Dict[str, Any]:
+    filter_operator = 'all' if 'and' in logical_operator else 'any'
+    converted_filters = []
+    for child_filter in child_filters:
+        converted_filter = _convert_query_filters_to_queryable_format(child_filter, entity_id)
+        converted_filters.append(converted_filter)
+    queryable_filter = {
+        "filter_operator": filter_operator,
+        "filters": converted_filters
+    }
+    return queryable_filter
+
+
+def _convert_simple_query_filters_to_queryable_format(filters: Dict[str, Any],
+                                                      entity_id: int) -> List[str]:
     is_filter_active = filters['active']
     if not is_filter_active:
         return []
@@ -88,21 +94,14 @@ def _convert_current_entity_values_to_passed_id(values: List[Dict[str, str]], en
 
 
 def _get_query_function_for_summary_type(summary_type: str) -> Callable[[str, List, str, str], Any]:
-    if summary_type == "average":
-        return _get_average_for_field
-    elif summary_type == "record_count":
-        return _get_record_count_for_field
+    if summary_type in ["average", "record_count"]:
+        return _summarize_field_for_summary_type
+    else:
+        raise TypeError(f"Summary type: {summary_type} not yet supported.")
 
 
-def _get_average_for_field(entity_type: str, filters: List[Union[List[str], Dict[str, Any]]],
-                           field: str, summary_type: str) -> int:
-    sg = get_shotgun_instance()
-    summary = sg.summarize(entity_type, filters, [{"field": field, "type": summary_type}])
-    return int(summary['summaries'][field])
-
-
-def _get_record_count_for_field(entity_type: str, filters: List[Union[List[str], Dict[str, Any]]],
-                                field: str, summary_type: str) -> int:
+def _summarize_field_for_summary_type(entity_type: str, filters: List[Union[List[str], Dict[str, Any]]],
+                                      field: str, summary_type: str) -> int:
     sg = get_shotgun_instance()
     summary = sg.summarize(entity_type, filters, [{"field": field, "type": summary_type}])
     return int(summary['summaries'][field])
